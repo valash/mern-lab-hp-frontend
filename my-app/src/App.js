1124bcd77a62f759e71297a8aafe0925a13bd0ed
Virginia Lash <virginialash1@gmail.com>
2019-03-11 16:49:39 -0400

    route paths and app page planned out

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import Create from './Components/Create';
+import Show from './Components/Show';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>hello harry potter LOVERS</h1>
+                <header className="App-header">
+                    <h1>hello harry potter LOVERS</h1>
+                </header>
+                <Route path="/" />
+                <Route path="/characters/:name" />
+                <Route path="/create" />
             </div>
         );
     }
